40bb9d6dae3c2d682456fc614dedeeb96f580470
Yegor <edeykin85@gmail.com>
2023-02-02 16:58:06 +0300

    added dependency lombok and implements in models User n Role. ManyToMany annot

--- src/main/java/com/example/SpringBoot_Security/model/Role.java
+++ src/main/java/com/example/SpringBoot_Security/model/Role.java
@@ -1,10 +1,33 @@
 package com.example.SpringBoot_Security.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
-//@Entity
-//@Table(name = "role")
-public class Role {
+import javax.persistence.*;
+import java.util.Set;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
 }
--- src/main/java/com/example/SpringBoot_Security/model/User.java
+++ src/main/java/com/example/SpringBoot_Security/model/User.java
@@ -1,72 +1,71 @@
 package com.example.SpringBoot_Security.model;
 
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+// TODO возможно выйдет ошибка в ManyToMany из-за (fetch = ...) и ломбок @getter
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "Users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "username", nullable = false)
+    private String username;
 
-    @Column(name = "surname")
-    private String surname;
+    @Column(name = "password", nullable = false)
+    private String password;
 
-    @Column(name = "age")
-    private int age;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
 
-    public User() {
-    }
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
 
-    public User(String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
     }
 
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", age=" + age +
-                '}';
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
