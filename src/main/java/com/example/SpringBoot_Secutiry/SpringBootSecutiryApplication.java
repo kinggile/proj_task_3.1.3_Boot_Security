21139d38bdbd47b712196c693abe223b48fcbc89
Yegor <edeykin85@gmail.com>
2023-02-02 14:31:06 +0300

    start ver of app

---
+++ src/main/java/com/example/SpringBoot_Secutiry/SpringBootSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Secutiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecutiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecutiryApplication.class, args);
+	}
+
+}
