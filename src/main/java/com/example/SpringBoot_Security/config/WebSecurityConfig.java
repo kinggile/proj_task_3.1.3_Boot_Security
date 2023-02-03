8b6fc976345ae85ef694fc9c84921b04bab7ce0f
Yegor <edeykin85@gmail.com>
2023-02-03 19:45:17 +0300

    app started properly. Auth worked properly. DB username: user, password: user (in BCrypt encoder)

--- src/main/java/com/example/SpringBoot_Security/config/WebSecurityConfig.java
+++ src/main/java/com/example/SpringBoot_Security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.SpringBoot_Security.config;
 
+import com.example.SpringBoot_Security.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -9,16 +10,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 //@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SuccessUserHandler successUserHandler;
+    private final UserServiceImpl userService;
 
     @Autowired
-    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserServiceImpl userService) {
         this.successUserHandler = successUserHandler;
+        this.userService = userService;
     }
 
     @Override
@@ -37,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    // аутентификация inMemory
+//     аутентификация inMemory
 //    @Bean
 //    @Override
 //    public UserDetailsService userDetailsService() {
@@ -52,7 +57,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //    }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userService);
 
+        return authenticationProvider;
+    }
+
+    // преобразователь паролей
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/SpringBoot_Security/service/UserServiceImpl.java
+++ src/main/java/com/example/SpringBoot_Security/service/UserServiceImpl.java
@@ -1,47 +1,52 @@
 package com.example.SpringBoot_Security.service;
 
+import com.example.SpringBoot_Security.model.Role;
 import com.example.SpringBoot_Security.model.User;
 import com.example.SpringBoot_Security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     // todo добавить транзакшионал на методы?
+
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    @Override
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+
+    public User findByUserName(String username) {
+        return userRepository.findByUsername(username);
     }
 
     @Override
-    public Optional<User> getOneUser(Long id) {
-        return userRepository.findById(id);
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
     }
 
-    @Override
-    public void updateUser(User user) {
-        userRepository.saveAndFlush(user); // todo ошибка уверен
-    }
-
-    @Override
-    public void saveUser(User user) {
-
-    }
-
-    @Override
-    public void deleteUser(User user) {
-
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList());
     }
 }
