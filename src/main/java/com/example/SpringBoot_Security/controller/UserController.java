a895580e9b1489a53983f81abe95939d0b808edd
Yegor <edeykin85@gmail.com>
2023-02-04 17:04:18 +0300

    add principal in user

--- src/main/java/com/example/SpringBoot_Security/controller/UserController.java
+++ src/main/java/com/example/SpringBoot_Security/controller/UserController.java
@@ -1,63 +1,18 @@
 package com.example.SpringBoot_Security.controller;
 
-import com.example.SpringBoot_Security.model.User;
-import com.example.SpringBoot_Security.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/user")
 public class UserController {
 
-    private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping()
-    public String showAllUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "admin/users";
-    }
-
-//    @GetMapping("/{id}")
-//    public String showUser(Model model, @PathVariable("id") int id) {
-//        model.addAttribute("user", userService.getOneUser(id));
-//        return "oneUser";
-//    }
-
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user) {
-        return "admin/newUser";
-    }
-
-    @PostMapping()
-    public String createNewUser(@ModelAttribute("user") User user) {
-        userService.save(user);
-
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String editUser(Model model, @PathVariable("id") Long id) {
-        model.addAttribute("user", userService.getOneUser(id));
-        return "admin/edit";
-    }
-
-    @PatchMapping("/edit/{id}")
-    public String editUser(@ModelAttribute("user") User user) {
-        userService.updateUser(user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
+    public String info(Principal principal) {
+        principal.getName();
+        return "user/userInfo";
     }
 }
