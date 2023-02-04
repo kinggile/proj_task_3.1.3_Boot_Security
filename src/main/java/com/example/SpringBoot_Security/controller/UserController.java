5932a991b8cefede015e700b61206e96a2a3a1e9
Yegor <edeykin85@gmail.com>
2023-02-04 17:39:15 +0300

    some changes in UserController

--- src/main/java/com/example/SpringBoot_Security/controller/UserController.java
+++ src/main/java/com/example/SpringBoot_Security/controller/UserController.java
@@ -1,20 +1,33 @@
 package com.example.SpringBoot_Security.controller;
 
+import com.example.SpringBoot_Security.repository.UserRepository;
+import com.example.SpringBoot_Security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import com.example.SpringBoot_Security.model.User;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+    private final UserRepository userRepository;
 
+    @Autowired
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
 
     @GetMapping()
-    public String info(Principal principal) {
-        principal.getName();
+    public String info(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
         return "user/userInfo";
     }
 }
