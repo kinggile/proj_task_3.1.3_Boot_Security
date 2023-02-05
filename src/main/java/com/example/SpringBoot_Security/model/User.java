cd6a79e350bce23b015e97059300620fde10836c
Yegor <edeykin85@gmail.com>
2023-02-05 15:46:47 +0300

    delete initialization hashset in model User

--- src/main/java/com/example/SpringBoot_Security/model/User.java
+++ src/main/java/com/example/SpringBoot_Security/model/User.java
@@ -38,7 +38,7 @@ public class User implements UserDetails {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
-    private Set<Role> roles = new HashSet<>();
+    private Set<Role> roles; // = new HashSet<>();
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
